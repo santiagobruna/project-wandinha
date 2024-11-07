f66ef4985e51850f00765885ca8f6123b84680b1
bruna <bruna@teste.com>
2024-11-07 19:11:02 -0300

    update project

--- gulpfile.js
+++ gulpfile.js
@@ -21,8 +21,9 @@ function scripts(){
     .pipe(gulp.dest('./dist/js'))
 }
 
-exports.default = gulp.parallel(styles, images, scripts);
+exports.default = exports.default = gulp.parallel(styles, images, scripts);
+
 exports.watch = function(){
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+    gulp.watch('./src/styles/**/*.scss', gulp.parallel(styles));
     gulp.watch('./src/scripts/*.js', gulp.parallel(scripts));
 }
